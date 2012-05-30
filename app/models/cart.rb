b82b45e781e88b68274d189a25ae427b406c9e68
shashank agarwal <sagarwal@sumeruadmin.(none)>
2012-05-30 19:30:20 +0530

    commit

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,20 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :cart_items
   has_many :products, :through => :cart_items
+
+
+  class TemporaryCart < Cart
+
+  def initialize session
+    @session = session[:user]
+  end
+
+  def add_item
+
+    @session[:temporary_cart][:num_items] += 1
+  end
+end
+
+
+
 end
