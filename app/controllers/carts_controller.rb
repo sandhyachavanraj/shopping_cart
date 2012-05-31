d6255e8a09539d34bd50d11581e25e3d214189b5
sandhya <ksandhya@user1-desktop.(none)>
2012-05-31 12:08:06 +0530

    changes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,10 +9,10 @@ class CartsController < ApplicationController
   end
 
   def create
+   
     @cart = Cart.find_or_create_by_user_id(current_user.id)    
-    @cart.product_ids = params[:id].split(',').collect{|n| n.to_i}
-    flash[:notice] = "success"
-    redirect_to  cart_items_path
+    @cart.product_ids = params[:id].split(',').collect{|n| n.to_i}    
+    redirect_to  cart_items_path, :alert => "success" 
   end
 end
 
