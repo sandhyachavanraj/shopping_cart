d2eda36c487cc04824a2a028db032aada93f1434
Shaskank <sagarwal@sumeruadmin.(none)>
2012-06-21 15:56:30 +0530

    committed

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,8 +3,8 @@ class Address < ActiveRecord::Base
   belongs_to :user
   belongs_to :user_profile
 
-  validates :mobile_number,:phone_number,:address,  :presence => true,:uniqueness => true
-  validates :phone_number, :length => {:in => 6..10}, :numericality => { :only_integer => true }
-  validates :mobile_number, :length => {:is => 10}, :numericality => { :only_integer => true }
-  
+  validates :mobile_number,:phone_number,:address,  :presence => true
+  validates :phone_number, :length => {:in => 6..10}, :numericality => { :only_integer => true, :message => "number only" }
+  validates :mobile_number, :length => {:is => 10}, :numericality => { :only_integer => true ,:message=> "number"}
+  validates :address, uniqueness => true
 end
