7d43b342a8330fa69210900354c4fc2cf39d826a
sandhya <ksandhya@user1-desktop.(none)>
2012-06-21 18:16:18 +0530

    conflicts resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,9 @@ class User < ActiveRecord::Base
   has_many :orders 
  
   #has_one :address , :as => :billing_address
- 
 
-
-  validates :name, :presence => true,  :format => {:with => /^[a-z0-9]{4}+[-a-z0-9]*[a-z0-9]+$/i }
-
-  validates :email, :presence => true,
-    :length => {:minimum => 3, :maximum => 254},
-
-    :format => {:with => /^([^@\s]{3,10}+)@((?:[-a-z0-9]{4,8}+\.)+[a-z]{2,4})$/i}
-
-
+  validates :name, :presence => true, :format => {:with => /^[a-z0-9]{4}+[-a-z0-9]*[a-z0-9]+$/i }
+  validates :email, :presence => true, :length => {:minimum => 3, :maximum => 254}, :format => {:with => /^([^@\s]{3,10}+)@((?:[-a-z0-9]{4,8}+\.)+[a-z]{2,4})$/i}
   validates :password, :presence =>true, :confirmation =>true, :length => { :minimum => 5, :maximum => 12}
   validates :password_confirmation, :presence => true,  :if => :password_changed?
   
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,11 +1,12 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user
-  has_many :addresses, :as => :adr, :dependent => :destroy
- # validates :image_url, :presence => true
-  accepts_nested_attributes_for :addresses ,:allow_destroy => true
+  
+  #  has_many :addresses, :as => :adr, :dependent => :destroy
+  # validates :image_url, :presence => true
+  #  accepts_nested_attributes_for :addresses ,:allow_destroy => true
 
-
-
+  has_one :billing_address, :as => :adr, :class_name => "Address"
+  has_one :shipping_address, :as => :adr, :class_name => "Address"
 
   def upload_image=(upload)
     self.image_url = upload.original_filename.to_s
@@ -14,7 +15,7 @@ class UserProfile < ActiveRecord::Base
     path = File.join(directory, upload.original_filename)
     File.open(path, "wb") { |f| f.write(upload.read) }
   end
-   def cleanup
+  def cleanup
     if File.exists?("#{Rails.root}/app/assets/images/#{self.image_url}")
       File.delete("#{Rails.root}/app/assets/images/#{self.image_url}")
     end
